75124ed055d2fdd497a9c539fd88fce9ca49e677
Fathir <fathiralattar29@gmail.com>
2024-09-25 11:19:50 +0700

    setengah fix

--- pemesanan.php
+++ pemesanan.php
@@ -98,7 +98,7 @@ while ($row = mysqli_fetch_assoc($result)) {
         
         <!-- Tampilkan notifikasi jika ada -->
         <?php if (isset($_SESSION['message'])): ?>
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <div class="alert alert-secondary alert-dismissible fade show" role="alert">
                 <?php echo $_SESSION['message']; ?>
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
